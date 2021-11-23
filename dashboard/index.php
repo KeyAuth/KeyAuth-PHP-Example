73d4678948d851c8ebc4ac5f2221da98f7781237
William Nelson <83034852+wnelson03@users.noreply.github.com>
2021-11-23 00:18:21 -0500

    500 error fix

--- dashboard/index.php
+++ dashboard/index.php
@@ -7,10 +7,7 @@ if (!isset($_SESSION['user_data'])) // if user not logged in
         header("Location: ../");
         exit();
 }
-
 $username = $_SESSION["user_data"]["username"];
-$subscription = $_SESSION["user_data"]["subscriptions"][0]->subscription;
-$expiry = $_SESSION["user_data"]["subscriptions"][0]->expiry;
 
 if(isset($_POST['logout']))
 {
@@ -27,9 +24,5 @@ if(isset($_POST['logout']))
 <body>
 <form method="post"><button name="logout">Logout</button></form>
 Logged in as <?php echo $username; ?>
-<br>
-Your Subscription:<?php echo $subscription; ?>
-<br>
-Your Subscription Expires: <script>document.write(convertTimestamp(<?php echo $expiry; ?>));</script>
 </body>
 </html>
