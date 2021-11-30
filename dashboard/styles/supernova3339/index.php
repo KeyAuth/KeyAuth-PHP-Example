76dd70de377ca0ba0a7939a134de98b487f87722
Supernova <63515814+Supernova3339@users.noreply.github.com>
2021-11-30 15:47:46 -0600

    Create index.php

---
+++ dashboard/styles/supernova3339/index.php
@@ -0,0 +1,12 @@
+<?php
+include 'main.php';
+?>
+
+<?=template_admin_header('Home')?>
+
+<h2>Home</h2>
+<div class="links">
+    <a href="https://github.com/Supernova3339">My GitHub Profile</a>
+</div>
+
+<?=template_admin_footer()?>
