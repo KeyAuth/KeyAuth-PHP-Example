4d3164b301b734c858f2ac4d0469826c7cc0c249
William Nelson <83034852+wnelson03@users.noreply.github.com>
2022-02-06 21:31:06 -0500

    Add example code to find subscription name

--- dashboard/index.php
+++ dashboard/index.php
@@ -1,5 +1,9 @@
 <?php
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 session_start();
 
 if (!isset($_SESSION['user_data'])) // if user not logged in
@@ -7,6 +11,18 @@ if (!isset($_SESSION['user_data'])) // if user not logged in
         header("Location: ../");
         exit();
 }
+
+function findSubscription($name, $list) {
+   for ($i = 0;$i < count($list);$i++)
+   {
+	 if ($list[$i]->subscription == $name)
+	 {
+		 return true;
+	 }
+	}
+   return false;
+}
+
 $username = $_SESSION["user_data"]["username"];
 $subscription = $_SESSION["user_data"]["subscriptions"][0]->subscription;
 $expiry = $_SESSION["user_data"]["subscriptions"][0]->expiry;
@@ -21,7 +37,7 @@ if(isset($_POST['logout']))
 <html>
 <head>
 <title>Dashboard</title>
-<script src="https://cdn.keyauth.com/dashboard/unixtolocal.js"></script>
+<script src="https://cdn.keyauth.win/dashboard/unixtolocal.js"></script>
 </head>
 <body>
 <form method="post"><button name="logout">Logout</button></form>
@@ -30,5 +46,7 @@ Logged in as <?php echo $username; ?>
 Your Subscription:<?php echo $subscription; ?>
 <br>
 Your Subscription Expires: <script>document.write(convertTimestamp(<?php echo $expiry; ?>));</script>
+<br>
+Does subscription with name <code style="background-color: #eee;border-radius: 3px;font-family: courier, monospace;padding: 0 3px;">default</code> exist: <?php echo ((findSubscription("default", $_SESSION["user_data"]["subscriptions"]) ? 1 : 0) ? 'yes' : 'no'); ?>
 </body>
 </html>
