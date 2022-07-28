86490796bf4a3e1f923a7b71fa96acebc377a90a
William Nelson <83034852+wnelson03@users.noreply.github.com>
2022-07-28 13:20:19 -0400

    Remove redundant version paramater

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ if (isset($_SESSION['user_data'])) {
 	exit();
 }
 
-$KeyAuthApp = new KeyAuth\api($name, $ownerid, $version);
+$KeyAuthApp = new KeyAuth\api($name, $ownerid);
 
 if (!isset($_SESSION['sessionid'])) {
 	$KeyAuthApp->init();
