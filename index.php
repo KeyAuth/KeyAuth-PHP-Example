982001c8d97af5725a49cfd01828502229c1043d
It's Networking <87145183+Its-Networking@users.noreply.github.com>
2023-12-03 02:11:26 -0500

    Update index.php
    Added new UI

--- index.php
+++ index.php
@@ -22,78 +22,138 @@ if (!isset($_SESSION['sessionid'])) {
 }
 ?>
 
-<html lang="en">
+<html lang="en" class="bg-[#09090d] text-white overflow-x-hidden">
 
 <head>
-        <title>KeyAuth PHP Example</title>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="shortcut icon" href="https://cdn.keyauth.cc/assets/img/favicon.png" type="image/x-icon">
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/notyf@3/notyf.min.css">
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" />
+    <title>KeyAuth PHP Example</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="shortcut icon" href="https://cdn.keyauth.cc/assets/img/favicon.png" type="image/x-icon">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/notyf@3/notyf.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" />
 
-        <link href="https://cdn.keyauth.cc/v2/assets/plugins/global/plugins.bundle.css" rel="stylesheet"
-                type="text/css">
-        <link href="https://cdn.keyauth.cc/v2/assets/css/style.bundle.css" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="https://cdn.keyauth.cc/v3/dist/output.css">
 </head>
 
-<body class="bg-dark">
-        <div class="d-flex flex-column flex-root">
-                <div
-                        class="d-flex flex-column flex-column-fluid bgi-position-y-bottom position-x-center bgi-no-repeat bgi-size-contain bgi-attachment-fixed">
-                        <div class="d-flex flex-center flex-column flex-column-fluid p-10 pb-lg-20">
-                                <div class="w-lg-500px bg-body rounded shadow-sm p-10 p-lg-15 mx-auto">
-                                        <form class="form w-100" method="post">
-                                                <div class="text-center mb-10">
-                                                        <h1 class="text-light mb-3">KeyAuth PHP Example</h1>
-                                                </div>
-
-                                                <div class="fv-row mb-10">
-                                                        <!-- username -->
-                                                        <label
-                                                                class="form-label fs-6 fw-bolder text-light">Username</label>
-                                                        <input class="form-control form-control text-light"
-                                                                type="text" name="username" placeholder="Enter username here" autocomplete="on" />
-                                                        <!-- password -->
-                                                                        <label
-                                                                                class="form-label fw-bolder text-light fs-6 mb-0">Password</label>
-                                                                <input class="form-control form-control text-light"
-                                                                        type="password" placeholder="Enter password here" name="password"
-                                                                        autocomplete="on" />
-                                                        <!-- License -->
-                                                                        <label
-                                                                                class="form-label fw-bolder text-light fs-6 mb-0">License</label>
-                                                                <input class="form-control form-control text-light"
-                                                                        type="text" placeholder="Enter license key" name="key" autocomplete="on" />
-                                                        <br>
-                                                        <div class="text-center">
-                                                                <button name="login"
-                                                                        class="btn btn-lg btn-primary w-100 mb-5">
-                                                                        <span class="indicator-label">Login</span>
-                                                                </button>
-                                                                <button name="register"
-                                                                        class="btn btn-lg btn-primary w-100 mb-5">
-                                                                        <span class="indicator-label">Register</span>
-                                                                </button>
-                                                                <button name="upgrade"
-                                                                        class="btn btn-lg btn-primary w-100 mb-5">
-                                                                        <span class="indicator-label">Upgrade</span>
-                                                                </button>
-                                                                <button name="license"
-                                                                        class="btn btn-lg btn-primary w-100 mb-5">
-                                                                        <span class="indicator-label">License</span>
-                                                                </button>
-                                                        </div>
-                                                </div>
-                                        </form>
-                                </div>
-                        </div>
+<body>
+    <header>
+        <nav class="border-gray-200 px-4 lg:px-6 py-2.5 mb-14">
+            <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
+                <a href="../" class="flex items-center">
+                    <img src="https://cdn.keyauth.cc/v2/assets/media/logos/logo-1-dark.png" class="mr-3 h-12 mt-2"
+                        alt="KeyAuth Logo" />
+                </a>
+                <div class="flex items-center lg:order-2">
+                    <a href="https://keyauth.cc" target="_blank"
+                        class="text-white focus:ring-0 font-medium rounded-lg text-sm px-4 py-2 lg:px-5 lg:py-2.5 mr-2 hover:opacity-60 transition duration-200 focus:outline-none focus:ring-gray-800">
+                        Help Via Ticket
+                    </a>
+                    <a href="https://t.me/keyauth" target="_blank"
+                        class="text-white focus:ring-0 font-medium rounded-lg text-sm px-4 py-2 lg:px-5 lg:py-2.5 mr-2 bg-blue-600 hover:opacity-80 focus:outline-none focus:ring-blue-800 transition duration-200">
+                        Help Via Telegram
+                    </a>
                 </div>
+            </div>
+        </nav>
+    </header>
+
+    <section>
+        <div class="relative z-10 flex flex-wrap md:-m-8 ml-8 md:ml-24">
+            <div class="w-full md:w-1/2 md:p-8">
+                <div class="md:max-w-lg md:mx-auto md:pt-36">
+                    <h2
+                        class="mb-7 md:mb-12 text-3xl md:text-6xl font-bold font-heading tracking-px-n leading-tight text-center">
+                        Welcome to the <span
+                            class="text-transparent bg-clip-text bg-gradient-to-r to-blue-600 from-sky-400 inline-block">KeyAuth
+                            PHP Example</span>
+                        👋
+                    </h2>
+
+                    <h3 class="mb-9 text-sm md:text-xl font-bold font-heading leading-normal">
+                        The best authentication platform for your software.
+                    </h3>
+                </div>
+            </div>
+            <div class="w-full md:w-1/2 md:p-8 -ml-4 md:-ml-12">
+                <div class="p-2 md:p-4 py-16 flex flex-col justify-center h-full">
+                    <form class="md:max-w-md md:ml-32 space-y-4 md:space-y-6" method="post" data-postform="1">
+                        <div class="relative mb-4" data-username="1">
+                            <input type="text" id="username" name="username"
+                                class="block px-2.5 pb-2.5 pt-4 w-full text-sm text-white bg-transparent rounded-lg border-1 border-border-gray-300 appearance-none focus:ring-0  peer"
+                                placeholder=" " autocomplete="on">
+                            <label for="username"
+                                class="absolute text-sm text-gray-400 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-[#09090d] px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 left-1">Username</label>
+                        </div>
+
+                        <div class="relative mb-4" data-password="1">
+                            <input type="password" id="password" name="password"
+                                class="block px-2.5 pb-2.5 pt-4 w-full text-sm text-white bg-transparent rounded-lg border-1 border-border-gray-300 appearance-none focus:ring-0  peer"
+                                placeholder=" " autocomplete="on">
+                            <label for="password"
+                                class="absolute text-sm text-gray-400 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-[#09090d] px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 left-1">Password</label>
+                        </div>
+
+                        <div class="relative mb-4" data-twofactor="1">
+                            <input type="text" id="key" name="key"
+                                class="block px-2.5 pb-2.5 pt-4 w-full text-sm text-white bg-transparent rounded-lg border-1 border-border-gray-300 appearance-none focus:ring-0  peer"
+                                placeholder=" " autocomplete="on">
+                                <label for="key"
+                                class="absolute text-sm text-gray-400 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-[#09090d] px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 left-1">Key</label>
+                        </div>
+
+                        <button name="login" data-loginbutton="1"
+                            class="text-white border-2 hover:bg-white hover:text-black focus:ring-0 focus:outline-none transition duration-200 font-medium rounded-lg text-sm px-5 py-2.5 text-center items-center mb-3 w-full mt-10">
+                            <span class="inline-flex">
+                                Login
+                                <svg class="w-3.5 h-3.5 ml-2 mt-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                                    fill="none" viewBox="0 0 14 10">
+                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                        stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"></path>
+                                </svg></span>
+                        </button>
+
+                        <button name="register" data-loginbutton="1"
+                            class="text-white border-2 hover:bg-white hover:text-black focus:ring-0 focus:outline-none transition duration-200 font-medium rounded-lg text-sm px-5 py-2.5 text-center items-center mb-3 w-full mt-10">
+                            <span class="inline-flex">
+                                Register
+                                <svg class="w-3.5 h-3.5 ml-2 mt-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                                    fill="none" viewBox="0 0 14 10">
+                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                        stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"></path>
+                                </svg></span>
+                        </button>
+
+                        <button name="upgrade" data-loginbutton="1"
+                            class="text-white border-2 hover:bg-white hover:text-black focus:ring-0 focus:outline-none transition duration-200 font-medium rounded-lg text-sm px-5 py-2.5 text-center items-center mb-3 w-full mt-10">
+                            <span class="inline-flex">
+                                Upgrade
+                                <svg class="w-3.5 h-3.5 ml-2 mt-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                                    fill="none" viewBox="0 0 14 10">
+                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                        stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"></path>
+                                </svg></span>
+                        </button>
+
+                        <button name="license" data-loginbutton="1"
+                            class="text-white border-2 hover:bg-white hover:text-black focus:ring-0 focus:outline-none transition duration-200 font-medium rounded-lg text-sm px-5 py-2.5 text-center items-center mb-3 w-full mt-10">
+                            <span class="inline-flex">
+                                License Register/Login
+                                <svg class="w-3.5 h-3.5 ml-2 mt-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                                    fill="none" viewBox="0 0 14 10">
+                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                        stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"></path>
+                                </svg></span>
+                        </button>
+
+                    </form>
+                </div>
+            </div>
         </div>
+    </section>
 
-        <script src="https://cdn.jsdelivr.net/npm/notyf@3/notyf.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/notyf@3/notyf.min.js"></script>
 
-        <?php
+    <?php
         if (isset($_POST['login'])) {
                 // login with username and password
                 if ($KeyAuthApp->login($_POST['username'], $_POST['password'])) {
@@ -127,4 +187,5 @@ if (!isset($_SESSION['sessionid'])) {
         ?>
 
 </body>
+
 </html>
